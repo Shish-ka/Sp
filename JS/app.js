ac91d89b99b77236828600848b90064d5184f44a
Shish-ka <milenaandreevna2007@gmail.com>
2025-02-20 13:23:16 +0500

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -5,16 +5,22 @@
 //const ul = document.querySelector ('ul')
 //ul.appendChild (li)
 
-const btn = document.querySelector ('#f')
-btn.addEventListener ('click', ()=> {
+const btn1 = document.querySelector ('#f')
+btn1.addEventListener ('click', ()=> {
     let input = document.querySelector ('input')
     if (input.value != 0) {
     const li = document.createElement ('li')
     li.innerHTML = input.value
     li.className = 'list-group-item'
+
+    const btn = document.createElement ('button')
+    btn.className = 'btn btn-danger'
+    btn.innerHTML = 'Удалить'
+
     const ul = document.querySelector ('ul')
-    ul.appendChild (li)}
+    ul.appendChild (li)
+
+    li.appendChild (btn)
+
     document.querySelector ('input').value = ''
-})
-
-
+}})
